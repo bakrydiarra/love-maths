784dd218fd62daa86f681640e466083a51343339
BakryDiarra <bakrydiarra@gmail.com>
2023-01-15 17:15:04 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,8 @@ function runGame(gameType) {
 
 if (gameType === "addition") {
     displayAdditonQuestion(num1, num2);
+} else if (gameType === "multiply") {
+    displayMultiplyQuestion(num1, num2);
 } else {
     alert(`unknown game type: ${gameType}`);
     throw `unknown game type: ${gameType}. Aborting!`;
@@ -69,6 +71,8 @@ let operator = document.getElementById("operator").innerText;
 
 if (operator === "+") {
     return [operand1 + operand2, "addition"];
+} else if (operator === "x") {
+    return [operand1 * operand2, "multiply"];
 } else {
     alert(`unimplemented operator ${operator}`);
     throw `unimplemented operator ${operator}. Aborting!`;
@@ -100,9 +104,11 @@ document.getElementById("operator").textContent = "+";
 }
 
 function diplaySubstractQuestion() {
-
+  
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 }
