eaf2e3a0b5df07128114a6fcf7b352b2866a5376
BakryDiarra <bakrydiarra@gmail.com>
2023-01-15 22:40:44 +0000

    Add divison display question

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,9 +40,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "substract") {
         diplaySubstractQuestion(num1, num2);
-    } else {
-        alert(`unknown game type: ${gameType}`);
-        throw `unknown game type: ${gameType}. Aborting!`;
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2)
     }
 
 }
@@ -85,9 +84,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "substract"]
-    } else {
-        alert(`unimplemented operator ${operator}`);
-        throw `unimplemented operator ${operator}. Aborting!`;
+    } else if (operator === "/") {
+       return [operand1 / operand2, "division"];
     }
 }
 
@@ -125,4 +123,15 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
+
+
+
 }
