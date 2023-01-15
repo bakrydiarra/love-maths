fdd1be8be86e5bcb60c9957a2cc41446eb83fab2
BakryDiarra <bakrydiarra@gmail.com>
2023-01-15 17:39:48 +0000

    Add code for substraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,8 @@ if (gameType === "addition") {
     displayAdditonQuestion(num1, num2);
 } else if (gameType === "multiply") {
     displayMultiplyQuestion(num1, num2);
+} else if (gameType === "substract") {
+    diplaySubstractQuestion(num1, num2);
 } else {
     alert(`unknown game type: ${gameType}`);
     throw `unknown game type: ${gameType}. Aborting!`;
@@ -73,6 +75,8 @@ if (operator === "+") {
     return [operand1 + operand2, "addition"];
 } else if (operator === "x") {
     return [operand1 * operand2, "multiply"];
+} else if (operator === "-") {
+    return [operand1 - operand2, "substract"]
 } else {
     alert(`unimplemented operator ${operator}`);
     throw `unimplemented operator ${operator}. Aborting!`;
@@ -103,8 +107,10 @@ document.getElementById("operand2").textContent = operand2;
 document.getElementById("operator").textContent = "+";
 }
 
-function diplaySubstractQuestion() {
-  
+function diplaySubstractQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
